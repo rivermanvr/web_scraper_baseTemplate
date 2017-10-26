ff604cdaef231c2e735a4a2a9fd9e4dda5994ac8
rivermanvr <vrgithub@riversconsulting.com>
2017-10-26 12:05:19 -0400

    setup..cont.

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const http = require( 'http' );
 const app = require( './app' );
-// const db = require( './db' );
 const server = http.createServer(app);
 const chalk = require( 'chalk' );
 
 const port = process.env.PORT || 2020;
-
-// db.seed()
-// .then(() => server.listen(port, () => console.log(chalk.yellow(`listening on port ${ port }`))))
-// .catch(err => {
-//   console.error(chalk.red('Error in sync/seed'));
-//   console.log(err);
-// });
 
 server.listen(port, () => console.log(chalk.yellow(`listening on port ${ port }`)));
