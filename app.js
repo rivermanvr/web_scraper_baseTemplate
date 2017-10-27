106da70c199fdff2a42966fafb142843623e11a3
rivermanvr <vrgithub@riversconsulting.com>
2017-10-27 09:23:38 -0400

    cant spend the time on this now

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const bodyParser = require('body-parser')
 const morgan = require( 'morgan' );
 const routes = require( './routes/api' );
 
+const webDriver = require( 'selenium-webdriver' ),
+  By = webDriver.By,
+  until = webDriver.until;
+
+const driver = new webDriver.Builder()
+  .forBrowser('chrome')
+  .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/public', express.static(path.join(__dirname, '/public')));
